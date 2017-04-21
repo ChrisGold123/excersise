e795b20bec938e952ace71d17f80f96f380c30ba
ChrisGold <pzukowwski@wp.pl>
2017-04-21 15:31:51 +0200

    cool thing

--- basic.js
+++ basic.js
@@ -39,7 +39,7 @@ if(hour===12 && prepand=== " AM ")
 
 if(second < 10)
 {
-	second = second.append('0');
+	second = second.toString().prepend('0');
 }
 
 document.getElementById("exeone").innerHTML = "Today is " + dayList[day] + ".<br>" + "The time is: " + hour + ":" + minute + ":" + second + " " + prepand;
